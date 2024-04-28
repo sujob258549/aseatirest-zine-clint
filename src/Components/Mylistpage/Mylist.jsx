88f6256c9bb2e3437403f46c34190975f6eec0ce
DevSujon <sujonmaia274@gmail.com>
2024-04-28 14:00:31 +0600

    table

--- src/Components/Mylistpage/Mylist.jsx
+++ src/Components/Mylistpage/Mylist.jsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import { CreatAuthContext } from "../Firebase/AuthContext";
-import { FaLocationDot } from "react-icons/fa6";
+import { FaEye, FaLocationDot, FaPen } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { TbDeviceIpadStar } from "react-icons/tb";
+import { MdDelete } from "react-icons/md";
 
 const Mylist = () => {
     const { user } = useContext(CreatAuthContext);
@@ -33,47 +34,46 @@ const Mylist = () => {
 
             <div className="py-10 md:py-16 lg:py-20">
                 <h2 className="text-xl md:text-5xl text-center font-bold">My torest add list page</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-10 md:py-16 ">
-                    {
-                        userTorestUserAdds.map(torestZone => (
-                            <div data-aos="fade-down" data-aos-duration="5000" key={torestZone.id} className='border-2 transition p-5 flex flex-col
-                     shadow  rounded-md hover'>
 
-                                <h2 className='text-xl px-5 font-semibold'>{torestZone.estate_title}</h2>
-                                <img className=' w-full  hover:transform hover:scale-110 duration-500 hover:rotate-6' src={torestZone.photourl} alt="" />
-                                <div className="py-5">
-                                    <p className='px-5'><span className="font-bold">Compani Name : </span> {torestZone.countryname}</p>
-                                    <p className=' px-5 py-2'><span className="font-bold">Average cost: </span> {torestZone.average_cost}</p>
-                                    <p className=' px-5 py-2'><span className="font-bold">Seasonality: </span> {torestZone.seasonality}</p>
-                                    <p className=' px-5 py-2'><span className="font-bold">Discribtion: </span> {torestZone.textArea}</p>
-
-                                </div>
-                                <hr />
-                                <div className='flex justify-end py-8 px-5'>
-                                    <div className='flex gap-3'>
-
-
-
-                                    </div>
-                                    <div className=' items-center flex gap-2'>
-                                        <FaLocationDot className='text-xl '> </FaLocationDot>
-                                        <p>{torestZone.location}</p>
-                                    </div>
-                                </div>
-                                <div>
-                                <div className="flex justify-end mr-5 flex-grow">
-                                    <Link to={`/detail/${torestZone._id}`} className="btn w-40   text-right text-white bg-[#400f0fd9] flex items-center hover:text-black"><TbDeviceIpadStar className="text-xl font-bold"></TbDeviceIpadStar> Vew detail</Link>
-                                </div>
-                                </div>
-
-                            </div>
-                        ))
-                    }
-                </div>
-                <div className="flex justify-center"> <Link to={`/alltorestaport`} className="btn w-40   text-right text-white bg-[#400f0fd9] flex items-center hover:text-black"><TbDeviceIpadStar className="text-xl font-bold"></TbDeviceIpadStar> Vew detail</Link></div>
-            </div>
+                <div className="overflow-x-auto py-10">
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                                <tr className="text-white">
+                                    <th></th>
+                                    <th className="text-xl font-bold">Email</th>
+                                    <th className="text-xl font-bold">User Name</th>
+                                    <th className="text-xl font-bold">countryname</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* row 1 */}
+                                {
+                                    userTorestUserAdds.map((user, index) => (
+                                        <tr key={user._id}>
+                                            <th>{index + 1}</th>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.countryname}</td>
+                                            <td>
+                                                <div>
+                                                <div className="tooltip" data-tip="Detete">
+                                                    <button  className="btn hover:bg-slate-400  bg-[#ea4744]">
+                                                        < MdDelete className="  text-white md:text-xl"></MdDelete>
+                                                    </button>
+                                                </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+           </div>  
         </div>
-    );
+    )
 };
 
 export default Mylist;
