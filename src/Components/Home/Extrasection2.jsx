aeb6fa4195493ce44c8bc473946a456120d453d2
DevSujon <sujonmaia274@gmail.com>
2024-05-01 04:24:16 +0600

    torest image section

--- src/Components/Home/Extrasection2.jsx
+++ src/Components/Home/Extrasection2.jsx
@@ -3,19 +3,38 @@ import Marquee from "react-fast-marquee";
 
 
 const Extrasection2 = () => {
-    return (
-       <div className="">
-        <div>
-        <div className="py-5">
-         <Marquee>
-           <div>
-           <img className="w-40" src="https://www.tourmyindia.com/blog//wp-content/uploads/2018/02/Cat-Ba-Island.jpg" alt="" />
-           </div>
-        </Marquee>
-       </div>
-        </div>
-       </div>
-    );
+   return (
+      <div className="">
+         <div>
+            <div className="py-5">
+               <h2 className="text-3xl font-bold md:text-5xl text-center pb-10 border-b mb-3 border-black">Torest Image section</h2>
+               <Marquee className="border-b border-black pb-3">
+                  <div className="flex gap-2">
+                     <img className="w-40 md:w-72" src="https://www.tourmyindia.com/blog//wp-content/uploads/2018/02/Cat-Ba-Island.jpg" alt="" />
+                     <img className="w-40 md:w-72" src="https://assets-news.housing.com/news/wp-content/uploads/2022/08/18174406/bangladesh-places-to-visit-FEATURE-compressed.jpg" alt="" />
+                     <img className="w-40 md:w-72" src="https://www.tourmyindia.com/blog//wp-content/uploads/2018/02/Cat-Ba-Island.jpg" alt="" />
+                     <img className="w-40 md:w-72" src="https://ihplb.b-cdn.net/wp-content/uploads/2020/10/Krabi-750x430.jpg" alt="" />
+                     <img className="w-40 md:w-72"  src="https://static.javatpoint.com/tourist-places/images/tourist-places-in-indonesia2.png" />
+                     <img className="w-40 md:w-72"  src="https://i.ytimg.com/vi/bCO3czGo0-E/maxresdefault.jpg" />
+                     <img className="w-40 md:w-72" src="https://www.tourmyindia.com/blog//wp-content/uploads/2018/02/Cat-Ba-Island.jpg" alt="" />
+                     <img className="w-40 md:w-72" src="https://miro.medium.com/v2/resize:fit:1200/1*syoDOUMMfMPkVl_uwinHug.jpeg" alt="" />
+                     <img className="w-40 md:w-72" src="https://assets-news.housing.com/news/wp-content/uploads/2022/08/18174406/bangladesh-places-to-visit-FEATURE-compressed.jpg" alt="" />
+                     <img className="w-40 md:w-72" src="https://www.tourmyindia.com/blog//wp-content/uploads/2018/02/Cat-Ba-Island.jpg" alt="" />
+                     <img className="w-40 md:w-72" src="https://ihplb.b-cdn.net/wp-content/uploads/2020/10/Krabi-750x430.jpg" alt="" />
+                     <img className="w-40 md:w-72"  src="https://static.javatpoint.com/tourist-places/images/tourist-places-in-indonesia2.png" />
+                     <img className="w-40 md:w-72"  src="https://i.ytimg.com/vi/bCO3czGo0-E/maxresdefault.jpg" />
+                     <img className="w-40 md:w-72" src="https://www.tourmyindia.com/blog//wp-content/uploads/2018/02/Cat-Ba-Island.jpg" alt="" />
+                     <img className="w-40 md:w-72" src="https://miro.medium.com/v2/resize:fit:1200/1*syoDOUMMfMPkVl_uwinHug.jpeg" alt="" />
+                     <img className="w-40 md:w-72" src="" alt="" />
+                     <img className="w-40 md:w-72" src="" alt="" />
+                     <img className="w-40 md:w-72" src="" alt="" />
+                 
+                  </div>
+               </Marquee>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Extrasection2;
